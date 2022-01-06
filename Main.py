f3653287a0c7b7b597c53eee52b820fd9a961690
orami01 <67837172+orami01@users.noreply.github.com>
2022-01-06 13:40:10 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,7 +12,8 @@ df = pandas.DataFrame(data)
 st.title('This is a First Streamlit App Test')
 st.subheader('This pages uses Streamlit and GitHub to publish a Flash Web App')
 st.write('''The data being used is random.
-The slider widget is being applied to simulate a temperature tool!
+The slider widget is being applied to simulate a temperature tool! 
+I am converting Celsius to Farenheight
 ''')
 st.write(df)
 st.line_chart(df)
